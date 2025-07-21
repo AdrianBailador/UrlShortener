3c60b5116754c0586298d034166b2431142ea596
Adrian Bailador <adrianbailador@hotmail.com>
2025-07-21 18:47:24 +0100

    Improve

--- obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
+++ obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f07bd34282faca8c048e046e1d748a1a6935c77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46592b982a29e98f12d47f7cb59f6875f54c183d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
