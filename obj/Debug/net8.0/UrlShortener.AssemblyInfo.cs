330ad85ae41472e909eabff0fa311c88cb9f5b0b
Adrian Bailador <adrianbailador@hotmail.com>
2025-07-21 17:57:35 +0100

    delete

--- obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
+++ obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862ab6c156b883429df43979ae5e6800dcce32f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f07bd34282faca8c048e046e1d748a1a6935c77")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
